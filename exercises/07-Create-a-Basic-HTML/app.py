05a6bf4eb7d3770db027704fde830f3e10c3c008
Nikkilabesf <powell.tenika.n@gmail.com>
2025-09-06 23:22:59 +0000

    Add wedding cost program

--- exercises/07-Create-a-Basic-HTML/app.py
+++ exercises/07-Create-a-Basic-HTML/app.py
@@ -10,4 +10,25 @@ h = '<body>'
 # ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
 
 # ✅ ↓ start coding below here ↓ ✅
+# Given variables
+html_open = "<html>"
+html_close = "</html>"
+head_open = "<head>"
+head_close = "<head>"
+body_open = "<body>"
+body_close = "<body>"
+title_open = "<title>"
+title_close = "</title>"
+
+# Concatenate in exact order
+# your 8 variables are already defined above
+html_document = e + c + g + a + f + h + d + b
+
+print(html_document)
+
+
+
+
+
+  
 
